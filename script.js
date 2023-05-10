6f4e78f66577a011162f02e7764570dd94b3441d
SRDFTM <59384975+SRDFTM@users.noreply.github.com>
2023-05-10 21:19:20 +0300

    scrambled letters added

--- script.js
+++ script.js
@@ -32,6 +32,49 @@ Array.from(document.getElementsByClassName("menu-item"))
   });
 
 
+// Получаем элемент <p> по его id
+const paragraph = document.getElementById('my-paragraph');
+// Получаем элемент <div> по его id
+const lettersDiv = document.getElementById('letters');
+
+// Получаем текст из элемента <p>
+const text = paragraph.textContent;
+
+// Разделяем текст на отдельные символы
+const characters = text.split('');
+
+// Очищаем содержимое элемента <p>
+paragraph.textContent = '';
+
+// Перебираем каждый символ и создаем для него новый <span> элемент
+characters.forEach((character) => {
+  const span = document.createElement('span');
+  span.textContent = character;
+  
+  // Генерируем случайные значения для позиции символа
+  const randomX = Math.random() * 100; // случайное значение от 0 до 100 для оси X
+  const randomY = Math.random() * 100; // случайное значение от 0 до 100 для оси Y
+  
+  // Применяем стили для случайной позиции символа
+  span.style.position = 'absolute';
+  span.className = 'letter-span';
+  span.style.left = `${randomX}%`;
+  span.style.top = `${randomY}%`;
+  
+  // Добавляем созданный <span> элемент внутрь элемента <div>
+  lettersDiv.appendChild(span);
+
+  lettersDiv.addEventListener('mouseover', () => {
+    // Восстанавливаем исходное положение символа
+    span.style.position = 'relative';
+    span.style.left = '';
+    span.style.top = '';
+    Array.from(document.getElementsByClassName("letter-span"))
+      .forEach((item) => {
+
+      })
+  });
+});
 
 
 
